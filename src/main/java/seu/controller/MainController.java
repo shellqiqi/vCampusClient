aa22a293ff011aeae2952061d7a750d3a8ee2f67
shellqiqi <shellqiqi@outlook.com>
2017-09-19 01:10:47 +0800

    update

--- src/main/java/seu/controller/MainController.java
+++ src/main/java/seu/controller/MainController.java
@@ -1,22 +1,7 @@
 package seu.controller;
 
 import de.felixroske.jfxsupport.FXMLController;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.web.WebView;
 
 @FXMLController
 public class MainController {
-
-    @FXML
-    private WebView mainWebView;
-
-    @FXML
-    private Button testButton;
-
-    public void test(ActionEvent event) {
-        mainWebView.getEngine().load("https://www.baidu.com");
-    }
 }
--- src/main/java/seu/controller/README.md
+++
@@ -1,1 +0,0 @@
-JavaFx controller
